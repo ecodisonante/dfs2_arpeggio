13e79679acf9abac98ccfe7f8a527b590a4312fe
Francisco Valdés <ecodisonante@gmail.com>
2024-06-01 17:42:27 -0400

    validar formularios on submit

--- js/login.js
+++ js/login.js
@@ -1,19 +1,44 @@
 
 
-// Crear listener para todos los campos form-control
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Crear listener para todos los campos form-control
     const formControls = document.querySelectorAll('.form-control');
     formControls.forEach(function (input) {
-        input.addEventListener('blur', validateField);
-        input.addEventListener('change', clearValidation);
+        input.addEventListener('blur', function (event) { validateField(event.target); });
+        input.addEventListener('keypress', function (event) { clearValidation(event.target); });
     });
+
+    // crear listener para todos los submit
+    document.addEventListener('submit', validateSubmit);
 });
 
+// Validar campos y continuar sumbit
+function validateSubmit(event) {
+    event.preventDefault(); 
 
-function validateField(event) {
-    console.log(event)
+    // Validar todos los campos del form antes de hacer submit
+    let isFormValid = true;
+    const inputs = event.target.querySelectorAll('.form-control');
 
-    let input = event.target;
+    console.log(inputs);
+
+    inputs.forEach(input => {
+        validateField(input);
+        isFormValid = isFormValid && input.checkValidity();
+    });
+
+    // de momento se simulara el submit
+    if (isFormValid) {
+        switch(event.target.id){
+            //TODO
+        }
+    }
+}
+
+
+// validar campo segun "data-val"
+function validateField(input) {
     let validation = input.dataset.val
     let fieldName = input.labels[0].innerHTML.replace(":", "");
     let validationMessage = "";
@@ -36,14 +61,16 @@ function validateField(event) {
 
     if (!validationMessage && validation == "repass") {
         let pass = document.getElementById("reg-password");
-        if (pass.value != repass.value) validationMessage =  "Las contraseñas no coinciden";
+        if (pass.value != repass.value) validationMessage = "Las contraseñas no coinciden";
     }
 
     input.setCustomValidity(validationMessage);
     input.reportValidity();
 }
 
-function clearValidation(event){
-    event.target.setCustomValidity("");
-    event.target.reportValidity();
+// limpiar mensaje cuando se modifica
+function clearValidation(input) {
+    input.setCustomValidity("");
+    input.reportValidity();
 }
+
